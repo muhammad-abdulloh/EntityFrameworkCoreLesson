eb2c38f2b39e9d9e651022b41dfdac52ba2e2215
Muhammad Abdulloh <clevercoderr@gmail.com>
2024-02-22 16:34:02 +0500

    change some

--- EntityFrameworkCoreLesson/Program.cs
+++ EntityFrameworkCoreLesson/Program.cs
@@ -14,11 +14,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-#if DEBUG
 builder.Services.AddScoped<ICarService, CarService>();
-#else 
-builder.Services.Singleton<ICarService, CarService>();
-#endif
+
 
 
 
@@ -28,12 +25,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 

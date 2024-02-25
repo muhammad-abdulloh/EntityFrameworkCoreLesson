318cb7d947123f93f4c322be1b18d74c65002b7d
muhammad-abdulloh <clevercoderr@gmail.com>
2024-02-25 11:42:02 +0000

    add action new release

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,20 @@
-echo Stopping entity1.service
-sudo systemctl stop entity1.service
+echo Stopping dotnet1.service
+sudo systemctl stop dotnet1.service
 
 echo Changing directory to project
-cd ~/dotnetfolder/EntityFrameworkCoreLesson
+cd ~/dotnetdeploy/EntityFrameworkCoreLesson
+
+echo Git Pulling
+git pull
 
 echo Removed old files
-sudo rm -rf /var/www/entity1/*
+sudo rm -rf /var/www/publishdll/*
 
 echo Publishing new app
 dotnet publish
 
 echo Copying new files
-sudo cp -r EntityFrameworkCoreLesson/bin/Release/net8.0/publish/* /var/www/entity1/
+sudo cp -r EntityFrameworkCoreLesson/bin/Release/net8.0/publish/* /var/www/publishdll/
 
 echo Starting service
-sudo systemctl start entity1.service
+sudo systemctl start dotnet1.service
